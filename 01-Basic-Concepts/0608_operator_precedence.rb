61d412e0c03c0eba65fd49524716e8c438b82237
Luis Angelo Rodrigues Jr <luisangelorjr@gmail.com>
2018-11-21 17:43:20 -0200

    Lesson 0608 completed

---
+++ 01-Basic-Concepts/0608_operator_precedence.rb
@@ -0,0 +1,23 @@
+=begin
+    PEMDMAS
+    P: Parentheses
+    E: Exponential
+    M: Multiplication
+    D: Division
+    M: Modulus
+    A: Addition
+    S: Subtraction
+=end
+
+x = (3+2) * 4
+puts x
+# Output is 20
+
+# Question
+puts 5 - 2 * 3 + 3
+#
+# a- 2*3
+# b- 5 - 6 + 3
+# c- -1 + 3
+#
+# Output is 2
